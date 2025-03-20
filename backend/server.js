05efeec953892419a6f423979d10f3e95d3d5912
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-20 13:07:23 -0700

    Add userRoute

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import mongoose from 'mongoose';
 import cors from "cors";
 import ticketRoute from "./Routes/ticketRoute.js";
+import userRoute from "./Routes/userRoute.js";
+
 dotenv.config()
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 app.use("/api/ticket" , ticketRoute);
+app.use("/api/user" , userRoute);
 mongoose.connect(process.env.MONGO_URI).then(
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
