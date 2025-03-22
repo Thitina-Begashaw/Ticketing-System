df11170db1014a53927400e4de4e604ac73b8fea
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-22 13:05:38 -0700

    Add frontend with talwind css configuration

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        <h1 className='text-4xl hover:text-red-500 text-blue-500'>CHECK ME</h1>
+      
+    </div>
+  )
+}
+
+export default HomePage
