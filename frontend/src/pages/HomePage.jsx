af24978973c8b9904c3ce7d6eb5639ed02b99f89
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-24 13:19:09 -0700

    Update home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HomeHeader from '../components/HomeHeader'
-
+import HomeBody1 from '../components/HomeBody1'
 function HomePage() {
   return (
     <div>
       <HomeHeader/>
+      <HomeBody1/>
       
       
     </div>
