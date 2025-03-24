37fe1f393723d56f85f74a4ec95244b4fd5d97e8
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-24 13:15:54 -0700

    Update the home page body

--- frontend/src/components/HomeBody1.jsx
+++ frontend/src/components/HomeBody1.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import SupportPic from "../assets/Support2.jpg";
 function HomeBody1() {
   return (
-    <div className=' bg-gray-100  h-screen shadow-sm -mt-4'>
+    <div className=' bg-gray-100  h-screen shadow-sm mt-7'>
 
-        <div className='float-left mt-28 ml-20'>
+        <div className='float-left mt-36 ml-20'>
             <h1 className='font-bold text-3xl'>Optimize Your Support with Our <br/>Advanced Ticketing System.</h1>
             <p className="text-gray-600 mt-5 text-xl ">Enhance efficiency with our intuitive support system. <br/> 
             Users can easily submit tickets, while administrators <br/> 
@@ -14,7 +14,7 @@ function HomeBody1() {
         </div>
 
         <div className='w-[500px] h-64 float-right mr-20 '>
-     <img className='mt-10 rounded-full shadow-xl'  src={SupportPic} alt="" />
+     <img className='mt-20 rounded-full shadow-xl'  src={SupportPic} alt="" />
      </div>
 
 
