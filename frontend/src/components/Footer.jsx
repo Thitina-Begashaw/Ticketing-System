0c3a56233a308553fea0017a6e9670d72aa4779a
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-25 13:30:49 -0700

    Add footer component on home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+     <h1 className='text-center m-8'>&copy; 2025 Developed by Thut Begashaw</h1>
+    </div>
+  )
+}
+
+export default Footer
