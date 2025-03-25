3cb8863a119d14d63980fec16b1c557683cdbef8
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-25 13:31:18 -0700

    Update Home page body

--- frontend/src/components/HomeBody1.jsx
+++ frontend/src/components/HomeBody1.jsx
@@ -5,7 +5,7 @@ function HomeBody1() {
     <div className=' bg-gray-100  h-screen shadow-sm mt-7'>
 
         <div className='float-left mt-36 ml-20'>
-            <h1 className='font-bold text-3xl'>Optimize Your Support with Our <br/>Advanced Ticketing System.</h1>
+            <h1 className='font-bold text-3xl'>Optimize Your Support with Our <br/>Advanced Ticketing System</h1>
             <p className="text-gray-600 mt-5 text-xl ">Enhance efficiency with our intuitive support system. <br/> 
             Users can easily submit tickets, while administrators <br/> 
             seamlessly manage and track issues, ensuring <br/> streamlined operations 
