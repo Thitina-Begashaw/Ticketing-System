f1421e2a742395e72ca8d491f5c7d2436a752058
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-03-25 13:31:40 -0700

    Update it

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import HomeHeader from '../components/HomeHeader'
 import HomeBody1 from '../components/HomeBody1'
+import Features from '../components/Features'
+import AboutUs from '../components/AboutUs'
+import Footer from '../components/Footer'
 function HomePage() {
   return (
     <div>
       <HomeHeader/>
       <HomeBody1/>
+      <Features/>
+      <AboutUs/>
+      <Footer/>
       
       
     </div>
