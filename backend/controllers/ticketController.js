4e2ac36d8e630830aaca74499269e2b5c95ee2d4
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-04-03 12:32:29 -0700

    update controller of ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 const createTicket = async (req, res) => {
   try {
     const { title, description } = req.body;
+    const { userId } = req.user;
     if (!title || !description) {
       return res
         .status(400)
         .json({ error: "Title and Description is required" });
     }
 
-    const ticket = await Ticket.create({ title, description });
+    const ticket = await Ticket.create({ title, description, userId });
     await ticket.save();
     res.status(200).json(ticket);
   } catch (error) {
@@ -26,18 +27,18 @@ const getTicket = async (req, res) => {
   }
 };
 
-const getPrivateTicket =async(req,res)=>{
-    const {id} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ message: "No Ticket with that id" });
-      }
+const getPrivateTicket = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "No Ticket with that id" });
+  }
 
-      try{
-            const ticket = await Ticket.findById(id);
-            res.status(200).json(ticket);
-      }catch(error){
-        res.status(500).json({message:"Error"})
-      }
+  try {
+    const ticket = await Ticket.find({ userId: id });
+    res.status(200).json(ticket);
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
 };
 
 const updateTicket = async (req, res) => {
@@ -74,4 +75,10 @@ const deleteTicket = async (req, res) => {
   }
 };
 
-export { createTicket, getTicket, updateTicket, deleteTicket , getPrivateTicket };
+export {
+  createTicket,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+  getPrivateTicket,
+};
