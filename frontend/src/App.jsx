474b4360b12c05416edb47aab9b40cecb6ce20b3
Thitina Begashaw <thitinabegashawtilahun@gmail.com>
2025-04-03 12:41:20 -0700

    Update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import InternalPage from './pages/InternalPage';
+import AddTicket from './components/AddTicket';
+import Dashboard from './components/Dashboard';
+import TicketList from './components/TicketList';
 function App() {
   return (
     <Router>
@@ -11,6 +15,12 @@ function App() {
           <Route path='/' element = {<HomePage/> }/>
           <Route path='/login' element = {<Login/> }/>
           <Route path='/signup' element = {<Signup/> }/>
+          <Route path='/user' element = {<InternalPage/> }/>
+          <Route path='/user/dashboard' element = {<Dashboard/> }/>
+
+
+          <Route path='/user/addticket' element = {<AddTicket/> }/>
+          <Route path='/user/myticket' element = {<TicketList/> }/>
         </Routes>
 
     </Router>
